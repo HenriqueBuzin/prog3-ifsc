19ff97e6825974c02b29c37ad64e87d9b738301c
Henrique Buzin <cultura.henrique@gmail.com>
2022-05-23 11:09:41 -0300

    Base

--- program/main.cpp
+++ program/main.cpp
@@ -15,16 +15,30 @@ class File {
 
 class Item {
     private:
-        int _quantity = 0;
-        std::string _name = "";
-        std::vector<File> _file = {};
+        std::string _name;
+        std::vector<File> _file;
+
+    protected:
+        int _quantity;
+
+        enum ItemType {
+            BOOK,
+            COMPONENT
+        };
+
+        ItemType _type;
+
     public:
-        void setQuantity(int quantity) {
-            _quantity = quantity;
-        }
-        int getQuantity() {
-            return _quantity;
-        }
+        Item(ItemType t): _type(t) { }
+
+        Item(ItemType t, int quantity): _type(t), _quantity(quantity) { }
+
+        Item(std::string name, ItemType t) : _name(name), _type(t) { }
+
+        virtual void setQuantity(int quantity) = 0;
+
+        virtual int getQuantity() = 0;
+
         void setName(std::string name) {
             _name = name;
         }
@@ -37,44 +51,89 @@ class Item {
         std::vector<File> getFile() {
             return _file;
         }
+
+        ItemType getType() {
+            return _type;
+        }
+
 };
 
 class Book: public Item {
     private:
-        int _isbn = 0;
+        int _isbn;
+
     public:
+        Book(): Item(BOOK, 1) {}
+
         void setIsbn(int isbn) {
             _isbn = isbn;
         }
         int getIsbn() {
             return _isbn;
         }
+
+        void setQuantity(int quantity){
+            std::cout << "Erro";
+        }
+
+        int getQuantity() {
+            return 1;
+        }
 };
 
 class Component: public Item {
     private:
-        int _voltage = 0;
+        int _voltage;
+
     public:
+        Component(): Item(COMPONENT) {}
+
         void setVoltage(int voltage) {
             _voltage = voltage;
         }
         int getVoltage() {
             return _voltage;
         }
+
+        void setQuantity(int quantity){
+            _quantity = quantity;
+        }
+
+        int getQuantity() {
+            return _quantity;
+        }
 };
 
 int main() {
-    std::cout << "Inicio!" << std::endl;
+    std::cout << "Inicio" << std::endl;
+
+    std::cout << "Livro" << std::endl;
 
     Book* book = new Book();
     book->setIsbn(1234);
-    book->setQuantity(1);
+    book->getType();
 
-    File* file = new File();
-    file->setUrl("google.com");
-    book->setFile(*file);
+    File* file0 = new File();
+    file0->setUrl("google.com");
+    book->setFile(*file0);
 
-    std:: cout << book->getIsbn() << std::endl;
+    std:: cout << book->getType() << std::endl;
+
+    std::cout << "-Livro-" << std::endl;
+
+    std::cout << "Componente" << std::endl;
+
+    Component* component = new Component();
+    component->setVoltage(400);
+    component->getType();
+
+    File* file1 = new File();
+    file1->setUrl("google.com");
+    book->setFile(*file1);
+
+    std:: cout << component->getType() << std::endl;
+
+    std::cout << "Componente" << std::endl;
 
     std::cout << "Fim!" << std::endl;
 
